6aa0f080c36d229e8b0b5b588fd8b9723802835e
Sanskar Gupta <72657115+sanskar0901@users.noreply.github.com>
2023-04-18 12:11:53 +0530

    damages field updated

--- models/claim.model.js
+++ models/claim.model.js
@@ -7,11 +7,11 @@ const ClaimSchema = new Schema(
         "policyId": {type:mongoose.Schema.Types.ObjectId, ref:'Policy'},
         "userId":{type:mongoose.Schema.Types.ObjectId, ref:'User'},
         "object": {
-          "damages":[{
+          "damages":{
             "crash%":{type:Number,default:0},
             "headlights%":{type:Number,default:0},
             "scratches%":{type:Number,default:0},
-        }],
+        },
           "img":{type:[String]}
         },
         "claimDate": {type:Date,default:Date.now()},
